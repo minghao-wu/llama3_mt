ee67879abb6e305e58f2041fc4dfed86a21e7c69
Minghao Wu <minghao.wu@monash.edu>
2024-05-11 16:30:44 +1000

    update

--- script.sh
+++ script.sh
@@ -19,12 +19,12 @@ module load cuda/12.1.0-gcc-8.5.0-5lkaqss
 
 nshots=$1
 
-python run_base.py \
-    --model_id meta-llama/Meta-Llama-3-8B \
-    --results_dir /home/minghaow/llamamt-project/llama3_mt/translations/$nshots-shot-base \
-    --nshots $nshots
-
-# python run_instruct.py \
-#     --model_id meta-llama/Meta-Llama-3-8B-Instruct \
-#     --results_dir /home/minghaow/llamamt-project/llama3_mt/translations/$nshots-shot-instruct \
+# python run_base.py \
+#     --model_id meta-llama/Meta-Llama-3-70B \
+#     --results_dir /home/minghaow/llamamt-project/llama3_mt/translations/$nshots-shot-base \
 #     --nshots $nshots
+
+python run_instruct.py \
+    --model_id meta-llama/Meta-Llama-3-70B-Instruct \
+    --results_dir /home/minghaow/llamamt-project/llama3_mt/translations/$nshots-shot-instruct \
+    --nshots $nshots
